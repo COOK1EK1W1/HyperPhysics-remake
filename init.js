8ce3ef62df96078725a83d07fd3297222a56ad04
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2022-02-07 16:05:33 +0000

    added grey out for menu

--- init.js
+++ init.js
@@ -1,11 +1,12 @@
-function showhidemenu(){
+function toggleMenu(){
     document.getElementById("topic-list").classList.toggle("active");
+    document.getElementById("grey-out").classList.toggle("active");
 }
 
 
 window.onload = function(){
     var burger = document.getElementById("hamburger"); //check for hamburger clicks
-    burger.addEventListener("click", showhidemenu);
+    burger.addEventListener("click", toggleMenu);
 
     document.location.hash = "";//stop targets
     
