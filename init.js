37a9dc204595f195f0bb944c4478e6319c98c92c
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2022-02-08 18:42:31 +0000

    changed a lot of the navigation stuff

--- init.js
+++ init.js
@@ -3,13 +3,31 @@ function toggleMenu(){
     document.getElementById("grey-out").classList.toggle("active");
 }
 
+function closeMenu(){
+    document.getElementById("topic-list").classList.remove("active");
+    document.getElementById("grey-out").classList.remove("active");
+}
+
 
 window.onload = function(){
     var burger = document.getElementById("hamburger"); //check for hamburger clicks
     burger.addEventListener("click", toggleMenu);
 
-    document.location.hash = "";//stop targets
-    
+    // document.location.hash = "";//stop targets
+
+
+    var toggler = document.getElementsByClassName("arrow");
+    for (var i = 0; i < toggler.length; i++) {
+    toggler[i].addEventListener("click", function() {
+        this.parentElement.querySelector(".nested").classList.toggle("active");
+        this.classList.toggle("arrow-down");
+    });
+    }
+
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i<links.length; i++){
+        links[i].addEventListener("click", closeMenu)
+    }
 }
 
 
